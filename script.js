2553be13aa4c440e7bc30157c4344ed0b262b6eb
Maryam Khalili <maryam.khalili@nyu.edu>
2020-10-01 11:24:26 +0400

    10/01

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+
 let bae = "good meowning"
 
 console.log(bae);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var element = document.createElement("button");
+    element.appendChild(document.createTextNode("Watch"));
+    var page = document.getElementById("btn");
+    page.appendChild(element);
+    console.log(element);
+
+
+});
+
+function setImageVisible(theImage, visible) {
+    var img = document.getElementById(theImage);
+    img.style.visibility = (visible ? 'visible' : 'hidden');
+}
+
 
