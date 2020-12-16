13ba84b0bf9d0463dd58f05c3829e667e13c08bb
Maryam Khalili <maryam.khalili@nyu.edu>
2020-12-17 00:05:23 +0300

    descriptions for projects and dro down menu

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+var navbar = document.querySelector(".navbar")
+var ham = document.querySelector(".ham")
 
-function showhide()
-{  
-        var div = document.getElementById("box3_1");  
-        if (div.style.display !== "none") 
-        {  
-            div.style.display = "none";  
-        }  
-        else
-        {  
-            div.style.display = "block";  
-        }  
-}  
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
+
+ham.addEventListener("click", toggleHamburger)
+
+// toggle when clicking on links
+
+
+var menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
+
+//Dear Professor, I tried creating a dropdown menu in all sorts of 
+//different ways but javascript decided to ignore all the toggles?? 
+//i ended up doing the dropdown with pure css
