829efc766540281e76ff67d82538da06742cb605
Maryam Khalili <maryam.khalili@nyu.edu>
2020-12-16 14:47:30 +0300

    updated the portfolio with comm lab projects

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 
-let bae = "good meowning"
-
-console.log(bae);
-
-/* the watch button */
-document.addEventListener("DOMContentLoaded", function() {
-    var element = document.createElement("button");
-    element.appendChild(document.createTextNode("Watch"));
-    var page = document.getElementById("btn");
-    page.appendChild(element);
-    console.log(element);
-
-
-});
-
-
-/*
-
-function setImageVisible(theImage, visible) {
-    var img = document.getElementById(theImage);
-    img.style.visibility = (visible ? 'visible' : 'hidden');
-}
-
-
-*/
-
-alert("thanks for visiting my portfolio! may the force be with you :)");
-
+function showhide()
+{  
+        var div = document.getElementById("box3_1");  
+        if (div.style.display !== "none") 
+        {  
+            div.style.display = "none";  
+        }  
+        else
+        {  
+            div.style.display = "block";  
+        }  
+}  
